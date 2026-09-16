b3b2a238f6f9063731cd2a4c54e75907b8194e12
panpetar405-creator <panpetar405@gmail.com>
2026-09-16 08:44:27 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const I18N = {
   sr: {
     nav_how:'Kako radi', nav_dest:'Destinacije', nav_about:'O nama',
     aria_account:'Nalog', aria_menu:'Meni',
-    hero_title:'SKLOPI <span class="accent">izlet</span>.',
+    hero_title:'SKLOPI <span class="accent">putovanje</span>.',
     hero_lede:'Sve u jednoj ceni.',
     label_origin:'Polazak', placeholder_origin:'npr. Beograd, Niš, Podgorica',
     label_dest:'Destinacija', placeholder_dest:'npr. Atina, Rim, Barselona',
